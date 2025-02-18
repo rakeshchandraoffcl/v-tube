5a1d6d16f76106d291ee44272f526c511263a6e9
rakesh <rakeshchandra.offcl@gmail.com>
2025-02-18 10:28:48 +0530

    Enhance tRPC setup with SuperJSON and authentication
    - Added SuperJSON transformer for improved data serialization
    - Integrated Clerk user authentication in tRPC context
    - Updated tRPC client and server configurations
    - Implemented protected tRPC procedures
    - Added server-side tRPC hydration helpers

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import { trpc } from "@/trpc/client";
+
 const page = () => {
+	const { data } = trpc.hello.useQuery({ text: "client" });
 	return (
 		<div>
 			<h1>Main section</h1>
+			<p>{data?.greeting}</p>
 		</div>
 	);
 };
--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,22 +1,42 @@
-import { initTRPC } from "@trpc/server";
+import superjson from "superjson";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { cache } from "react";
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/db";
+import { users } from "@/db/schema";
+import { eq } from "drizzle-orm";
 export const createTRPCContext = cache(async () => {
 	/**
 	 * @see: https://trpc.io/docs/server/context
 	 */
-	return { userId: "user_123" };
+	const user = await auth();
+	return { clerkUserId: user.userId };
 });
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>;
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create({
+const t = initTRPC.context<Context>().create({
 	/**
 	 * @see https://trpc.io/docs/server/data-transformers
 	 */
-	// transformer: superjson,
+	transformer: superjson,
 });
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(async ({ ctx, next }) => {
+	if (!ctx.clerkUserId) {
+		throw new TRPCError({ code: "UNAUTHORIZED" });
+	}
+	const [user] = await db
+		.select()
+		.from(users)
+		.where(eq(users.clerkId, ctx.clerkUserId));
+	if (!user) {
+		throw new TRPCError({ code: "UNAUTHORIZED" });
+	}
+	return next({ ctx: { ...ctx, user } });
+});
